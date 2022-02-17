aadd40c3c83011d64ab1853166b1317701dd4d9e
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-02-17 17:42:56 -0400

    mostrando a traves de las vistas los titulos de los proyectos con la sintaxis rara que ofrece django para la parte del front

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from models import Project
+
 # Create your views here.
+from portfolio.models import Project #asi se tiene que importar de manera correcta un modelo
+
 
 def home(request):
-    return render(request, 'home.html')
+   projects = Project.objects.all() #esta variable almacena todos los proyectos del objeto
+   return render(request, 'home.html',{'projects':projects})
